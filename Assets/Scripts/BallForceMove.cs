dde2d43dc28118806fa4dd4ed967d244b864218b
gupi1unity <anton.korolev.06@mail.ru>
2024-09-20 11:18:52 +0500

    ball script changes

--- Assets/Scripts/BallForceMove.cs
+++ Assets/Scripts/BallForceMove.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class BallForceMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveForce = 5.0f;
+
+    private Rigidbody _rigidbody;
+    private Camera _camera;
+
+    private bool _isMoving;
+
+    private string _horizontalAxisName = "Horizontal";
+    private string _verticalAxisName = "Vertical";
+    private Vector3 _inputVector;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _camera = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_inputVector != Vector3.zero)
+        {
+            _isMoving = true;
+        }
+        else
+        {
+            _isMoving = false;
+        }
+
+        HandleMoveDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMoving == true)
+        {
+            _rigidbody.AddForce(_inputVector * _moveForce, ForceMode.Force);
+        }
+    }
+
+    private void HandleMoveDirection()
+    {
+        float xInput = Input.GetAxisRaw(_horizontalAxisName);
+        float yInput = Input.GetAxisRaw(_verticalAxisName);
+
+        _inputVector = new Vector3(xInput,0,yInput).normalized;
     }
 }
